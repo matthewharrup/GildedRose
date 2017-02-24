495b55158eb8a849a85fa1dc2a893b489aee4dd4
matt <matt@Beast>
2017-02-24 15:20:59 +0000

    Major refactoring based around subclassing item types (an AgedBrie type, a Sulfuras type) with a QualityControl factory to pick the appropriate class based of item name (as stored in com.GildedRose.ItemNames).
    Now the main method goes from 70 lines of code to 7 and the logic is
    devolved to the individual classes
    
    Note due to time constraints only Sulfuras and AgedBrie classes are
    here; backstage passes, conjured items etc still to come

--- src/test/java/GildedRoseTest.java
+++ src/test/java/GildedRoseTest.java
@@ -4,17 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
+import com.GildedRose.*;
 
+import com.GildedRose.QualityControl.*;
+import com.GildedRose.SellInControl.*;
 
 public class GildedRoseTest {
 
+	private QualityControlFactory qcf = new QualityControlFactory();
+	private SellInControl sic = new SellInControl();
+	
+	
 	@Test
 	public void testDexVestQualityDecreases() {
         List<Item> items = new ArrayList<Item>();
         items.add(new Item("+5 Dexterity Vest", 10, 20));
-        GildedRose gr = new GildedRose();
-        gr.setItems(items);
-        items =gr.updateQuality();
+        GildedRose gr = new GildedRose(qcf, sic);
+        //
+        gr.updateQuality(items);
         assertEquals(items.get(0).getQuality(), 19);
         
 	}
@@ -23,9 +30,9 @@ public class GildedRoseTest {
 	public void testDexVestSellInDecreases() {
         List<Item> items = new ArrayList<Item>();
         items.add(new Item("+5 Dexterity Vest", 10, 20));
-        GildedRose gr = new GildedRose();
-        gr.setItems(items);
-        items =gr.updateQuality();
+        GildedRose gr = new GildedRose(qcf, sic);
+        //
+        gr.updateQuality(items);
         assertEquals(items.get(0).getSellIn(), 9);
         
 	}
@@ -33,9 +40,9 @@ public class GildedRoseTest {
 	public void testDexVestExpiredQualityDecreases() {
         List<Item> items = new ArrayList<Item>();
         items.add(new Item("+5 Dexterity Vest", -1, 20));
-        GildedRose gr = new GildedRose();
-        gr.setItems(items);
-        items =gr.updateQuality();
+        GildedRose gr = new GildedRose(qcf,sic);
+        
+        gr.updateQuality(items);
         assertEquals(items.get(0).getQuality(), 18);
         
 	}	
@@ -43,9 +50,9 @@ public class GildedRoseTest {
 	public void testDexVestNonNegativeQuality() {  
         List<Item> items = new ArrayList<Item>();
         items.add(new Item("+5 Dexterity Vest", -1, 0));
-        GildedRose gr = new GildedRose();
-        gr.setItems(items);
-        items =gr.updateQuality();
+        GildedRose gr = new GildedRose(qcf,sic);
+        
+        gr.updateQuality(items);
         assertEquals(items.get(0).getQuality(), 0); //quality would normally be decremented by 1 to -1, but can never go below zero
         
 	}	
@@ -53,9 +60,9 @@ public class GildedRoseTest {
 	public void testAgedBrieGoesUpInQuality() {  
         List<Item> items = new ArrayList<Item>();
         items.add(new Item("Aged Brie", 2, 0));
-        GildedRose gr = new GildedRose();
-        gr.setItems(items);
-        items =gr.updateQuality();
+        GildedRose gr = new GildedRose(qcf,sic);
+        
+        gr.updateQuality(items);
         assertEquals(items.get(0).getQuality(), 1); 
         
 	}		
@@ -63,9 +70,9 @@ public class GildedRoseTest {
 	public void testQualityNeverMoreThanFifty() {  
         List<Item> items = new ArrayList<Item>();
         items.add(new Item("Aged Brie", 2, 50));
-        GildedRose gr = new GildedRose();
-        gr.setItems(items);
-        items =gr.updateQuality();
+        GildedRose gr = new GildedRose(qcf,sic);
+        
+        gr.updateQuality(items);
         assertEquals(items.get(0).getQuality(), 50); 
         
 	}	
@@ -73,9 +80,9 @@ public class GildedRoseTest {
 	public void testSulfurasNeverChangesQuality(){  
         List<Item> items = new ArrayList<Item>();
         items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
-        GildedRose gr = new GildedRose();
-        gr.setItems(items);
-        items =gr.updateQuality();
+        GildedRose gr = new GildedRose(qcf,sic);
+        
+        gr.updateQuality(items);
         assertEquals(items.get(0).getQuality(), 80); 
         
 	}	
@@ -83,9 +90,9 @@ public class GildedRoseTest {
 	public void testSulfurasNeverChangesSellin(){  
         List<Item> items = new ArrayList<Item>();
         items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
-        GildedRose gr = new GildedRose();
-        gr.setItems(items);
-        items =gr.updateQuality();
+        GildedRose gr = new GildedRose(qcf,sic);
+        
+        gr.updateQuality(items);
         assertEquals(items.get(0).getSellIn(), 0);
         
 	}		
@@ -93,9 +100,9 @@ public class GildedRoseTest {
 	public void testBackstagePassIncreasesInQualityOverTenDays(){  
         List<Item> items = new ArrayList<Item>();
         items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
-        GildedRose gr = new GildedRose();
-        gr.setItems(items);
-        items =gr.updateQuality();
+        GildedRose gr = new GildedRose(qcf,sic);
+        
+        gr.updateQuality(items);
         assertEquals(items.get(0).getQuality(), 21);
         
 	}	
@@ -103,9 +110,9 @@ public class GildedRoseTest {
 	public void testBackstagePassIncreasesInQualityDoublyUnderTenDays(){  
         List<Item> items = new ArrayList<Item>();
         items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 9, 20));
-        GildedRose gr = new GildedRose();
-        gr.setItems(items);
-        items =gr.updateQuality();
+        GildedRose gr = new GildedRose(qcf,sic);
+        
+        gr.updateQuality(items);
         assertEquals(items.get(0).getQuality(), 22); //should go up by 2 if between 6-10 days left
 	}		
 	
@@ -113,9 +120,9 @@ public class GildedRoseTest {
 	public void testBackstagePassIncreasesInQualityTriplyUnderFiveDays(){  
         List<Item> items = new ArrayList<Item>();
         items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20));
-        GildedRose gr = new GildedRose();
-        gr.setItems(items);
-        items =gr.updateQuality();
+        GildedRose gr = new GildedRose(qcf,sic);
+        
+        gr.updateQuality(items);
         assertEquals(items.get(0).getQuality(), 23); //should go up by 3 if between 1-5 days left
 	}		
 	
@@ -123,9 +130,9 @@ public class GildedRoseTest {
 	public void testBackstagePassDropsQualityAfterConcert(){  
         List<Item> items = new ArrayList<Item>();
         items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20));
-        GildedRose gr = new GildedRose();
-        gr.setItems(items);
-        items =gr.updateQuality();
+        GildedRose gr = new GildedRose(qcf,sic);
+        
+        gr.updateQuality(items);
         assertEquals(items.get(0).getQuality(), 0); 
 	}	
 	
@@ -133,18 +140,18 @@ public class GildedRoseTest {
 	public void testConjuredItemsDecreaseTwiceAsFast(){  
         List<Item> items = new ArrayList<Item>();
 		items.add(new Item("Conjured Mana Cake", 3, 6));
-        GildedRose gr = new GildedRose();
-        gr.setItems(items);
-        items =gr.updateQuality();
+        GildedRose gr = new GildedRose(qcf,sic);
+        
+        gr.updateQuality(items);
         assertEquals(items.get(0).getQuality(), 4); //4, because this would normally degrade by 1 except twice as fast as it's "conjured"
 	}		
 	@Test
 	public void testConjuredItemsDecreaseTwiceAsFastAfterSellin(){  
         List<Item> items = new ArrayList<Item>();
 		items.add(new Item("Conjured Mana Cake", -1, 6));
-        GildedRose gr = new GildedRose();
-        gr.setItems(items);
-        items =gr.updateQuality();
+        GildedRose gr = new GildedRose(qcf,sic);
+        
+        gr.updateQuality(items);
         assertEquals(items.get(0).getQuality(), 2); //2, because it's conjured and expired hence four times faster degradation
 	}		
 	
